fb245e0f35b54f9914ee0ebe6e65c348249aeb16
isaacNuflo <isaac.nuflo21@gmail.com>
2019-01-27 23:53:24 -0500

    schema

---
+++ server/routes/usuario.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const Usuario = require('../models/usuario');
+
+const app = express();
+const bcrypt = require('bcrypt');
+
+app.get('/usuario', (req, res) => {
+    res.json('Get usuario');
+});
+
+app.post('/usuario', (req, res) => {
+
+    let body = req.body;
+
+    let usuario = new Usuario({
+        nombre: body.nombre,
+        email: body.email,
+        password: bcrypt.hashSync(body.password, 10),
+        role: body.role
+    });
+
+    usuario.save((err, usuarioDB) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        //usuarioDB.password = null;
+
+        res.json({
+            ok: true,
+            usuario: usuarioDB
+        });
+    });
+
+});
+
+app.put('/usuario/:id', (req, res) => {
+
+    let id = req.params.id;
+    res.json({
+        id
+    });
+
+});
+
+app.delete('/usuario', (req, res) => {
+    res.json('Delete usuario');
+});
+
+module.exports = app;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 require('./config/config');
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -9,38 +10,11 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.get('/usuario', (req, res) => {
-    res.json('Get usuario');
-});
+app.use(require('./routes/usuario'));
 
-app.post('/usuario', (req, res) => {
-
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    } else {
-        res.json({
-            persona: body
-        });
-    }
-
-});
-
-app.put('/usuario/:id', (req, res) => {
-
-    let id = req.params.id;
-    res.json({
-        id
-    });
-
-});
-
-app.delete('/usuario', (req, res) => {
-    res.json('Delete usuario');
+mongoose.connect('mongodb://isaac.nuflo:prodequa123@ds111025.mlab.com:11025/landing', { useNewUrlParser: true }, (err, res) => {
+    if (err) throw err;
+    console.log('Base de datos ONLINE');
 });
 
 app.listen(process.env.PORT, () => {
